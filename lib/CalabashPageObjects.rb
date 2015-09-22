8f90768f56642cf62c6e3210d214968f566c279e
Alan Nichols <alan.nichols@just-eat.com>
2015-09-22 12:12:11 +0100

    Moved the code that is shared between the base classes into a shared module

--- lib/CalabashPageObjects.rb
+++ lib/CalabashPageObjects.rb
@@ -1,6 +1,7 @@
 require 'CalabashPageObjects/version'
 require 'CalabashPageObjects/calabash_android_base'
 require 'CalabashPageObjects/calabash_ios_base'
+require 'CalabashPageObjects/shared_methods'
 require 'calabash-android'
 require 'calabash-cucumber'
 
