e0641d78ff895d60f1e3a8c0b8d698b5904e7b4f
Alan Nichols <alan.nichols@just-eat.com>
2015-09-24 16:10:13 +0100

    Fixage

--- lib/CalabashPageObjects/shared_methods.rb
+++ lib/CalabashPageObjects/shared_methods.rb
@@ -5,55 +5,55 @@ module SharedMethods
   # rubocop:disable NestedMethodDefinition
   CPO_LOGGING ||= false
 
-  def options_parser(input_options, defaults = {})
-    if input_options.is_a?(Integer)
-      timeout_only_output_options = {}
-      timeout_only_output_options[:timeout] = input_options
-      timeout_only_output_options
-    else
-      defaults.merge(input_options)
-    end
-  end
-
-  def find(element_name, timeout, parent, webview)
-    puts "Looking for #{element_name} with an initial delay of #{timeout} seconds." if CPO_LOGGING
-    return true if send("#{element_name}_present?", timeout: timeout)
-
-    parent = "webview css:'*'" if webview # if looking for an element in a webview, the scroll parent is now a webview selector
-    puts 'Element has not been found within this timeout. Scrolling...' if CPO_LOGGING
-
-    element_present = false
-    webview ? current_screen_state = query("webview css:'*'") : current_screen_state = query('*')
-    prev_screen_state = []
-
-    while !element_present && current_screen_state != prev_screen_state
-      prev_screen_state = current_screen_state
-
-      if parent.nil?
-        begin
-          puts 'Scrolling down normally' if CPO_LOGGING
-          scroll_down
-        rescue
-          puts "View is not currently scrollable after a wait of #{timeout}"
-        end
+  def element(element_name, locator)
+    define_method('options_parser') do |input_options, defaults = {}|
+      if input_options.is_a?(Integer)
+        timeout_only_output_options = {}
+        timeout_only_output_options[:timeout] = input_options
+        timeout_only_output_options
       else
-        puts "Scrolling down parent #{parent}" if CPO_LOGGING
-        scroll(Send('parent_locator'), :down)
+        defaults.merge(input_options)
+      end
+    end
+
+    define_method('find') do |element_name, timeout, parent, webview|
+      puts "Looking for #{element_name} with an initial delay of #{timeout} seconds." if CPO_LOGGING
+      return true if send("#{element_name}_is_present?", {timeout: timeout, scroll: false})
+
+      parent = "webview css:'*'" if webview # if looking for an element in a webview, the scroll parent is now a webview selector
+      puts 'Element has not been found within this timeout. Scrolling...' if CPO_LOGGING
+
+      element_present = false
+      webview ? current_screen_state = query("webview css:'*'") : current_screen_state = query('*')
+      prev_screen_state = []
+
+      while !element_present && current_screen_state != prev_screen_state
+        prev_screen_state = current_screen_state
+
+        if parent.nil?
+          begin
+            puts 'Scrolling down normally' if CPO_LOGGING
+            scroll_down
+          rescue
+            puts "View is not currently scrollable after a wait of #{timeout}"
+          end
+        else
+          puts "Scrolling down parent #{parent}" if CPO_LOGGING
+          scroll(Send('parent_locator'), :down)
+        end
+
+        sleep 1
+        element_present = send("#{element_name}_present?")
+        puts "Is element present? => #{element_present}" if CPO_LOGGING
+
+        webview ? current_screen_state = query("webview css:'*'") : current_screen_state = query('*')
+        puts 'Have reached the bottom of the screen.' if current_screen_state == prev_screen_state && CPO_LOGGING
+        puts 'Have not reached the bottom of the screen.  Retrying.' if current_screen_state != prev_screen_state && CPO_LOGGING
       end
 
-      sleep 1
-      element_present = send("#{element_name}_present?")
-      puts "Is element present? => #{element_present}" if CPO_LOGGING
-
-      webview ? current_screen_state = query("webview css:'*'") : current_screen_state = query('*')
-      puts 'Have reached the bottom of the screen.' if current_screen_state == prev_screen_state && CPO_LOGGING
-      puts 'Have not reached the bottom of the screen.  Retrying.' if current_screen_state != prev_screen_state && CPO_LOGGING
+      element_present
     end
 
-    element_present
-  end
-
-  def element(element_name, locator)
     define_method(element_name.to_s) do
       query("#{locator}")
     end
@@ -71,8 +71,18 @@ module SharedMethods
     end
 
     define_method("#{element_name}_is_present?") do |options = {}|
-      opts = options_parser(options, timeout: 0, parent: nil, webview: false)
-      find(element_name, opts[:timeout], opts[:parent], opts[:webview])
+      opts = options_parser(options, timeout: 0, parent: nil, webview: false, scroll: false)
+      if opts[:scroll]
+        find(element_name, opts[:timeout], opts[:parent], opts[:webview])
+      else
+        puts "Checking for the presence of #{element_name} after #{opts[:timeout]} seconds..." if CPO_LOGGING
+        begin
+          wait_for_element_exists(locator, timeout: opts[:timeout], screenshot_on_error: false)
+          true
+        rescue
+          false
+        end
+      end
     end
 
     define_method("touch_#{element_name}") do |options = {}|
@@ -129,18 +139,6 @@ module SharedMethods
 
       unless find(element_name, opts[:timeout], opts[:parent], opts[:webview])
         fail @wait_error, "Timeout waiting for elements: #{locator}"
-      end
-    end
-
-    define_method("#{element_name}_present?") do |options = {}|
-      opts = options_parser(options, timeout: 1)
-      puts "Checking for the presence of #{element_name} after #{opts[:timeout]} seconds..." if CPO_LOGGING
-
-      begin
-        wait_for_element_exists(locator, timeout: opts[:timeout], screenshot_on_error: false)
-        true
-      rescue
-        false
       end
     end
 
